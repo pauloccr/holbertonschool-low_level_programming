1728a379bcc80871ae1b809ab5f04a5b3dd5701b
pauloccr <4733@holbertonschool.com>
2022-02-18 21:12:24 -0500

    Debugging - first steps

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 
+void print_remaining_days(int month, int day, int year);
+
 int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
