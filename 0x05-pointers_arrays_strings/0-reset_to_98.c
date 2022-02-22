5d544dea88db66f7250c93ddbfd5a716f1a43c89
pauloccr <4733@holbertonschool.com>
2022-02-21 23:37:44 -0500

    first

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - takes a pointer to an int as parameter and updates the value
+ * it points to 98
+ * Return: 0
+ */
+
+int main(void);
+
+{
+	int n = 402;
+	int *p = &n;x
+	printf("number is %p\n", p);
+	*p = 98;
+	printf("number is %p\n", p);
+	return (0);
+}
